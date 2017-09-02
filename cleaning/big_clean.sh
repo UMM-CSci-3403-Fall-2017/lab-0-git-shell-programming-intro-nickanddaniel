cd2ff414b7c618c64141ae5554c296392c719458
bushw011 <bushw011@morris.umn.edu>
2017-09-01 19:13:04 -0500

    fixed the path so that so that it ACTUALLY works this time. i swear it works, this isn't like the last times where I said it works....I promise.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,10 +3,13 @@
 arch=$1
 scratch=$2
 
+here=$(pwd)
+echo $here
+
 tar -xzf $arch -C $scratch
 
 grep -lr "DELETE ME!" $scratch | xargs rm -f
 
 cd $scratch
 
-tar -zcf ../"cleaned_"$arch ${arch%.*}
+tar -zcf $here/"cleaned_"$arch ${arch%.*}
